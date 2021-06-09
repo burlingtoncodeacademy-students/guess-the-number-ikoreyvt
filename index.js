9e7cbe2e757e13e78061f77a0339e5d4b8d91e9c
unknown <ikoreyvt@gmail.com>
2021-06-09 16:59:09 -0400

    made a few comments on the project

--- index.js
+++ index.js
@@ -7,12 +7,33 @@ function ask(questionText) {
   });
 }
 
-start();
+//initiate a couple variables that dictate the range of the game
+let rangeMax = 100;
+let rangeMin = 1;
 
-async function start() {
-  console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
-  let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
-  console.log('You entered: ' + secretNumber);
-  // Now try and complete the program.
-  process.exit();
+//create a function that will choose random number based on the range
+
+function randomNum(rangeMax) {
+  
 }
+
+
+/*after getting the input, provide a random number between the range to check against the player's guess
+
+if the number is correct, end the game and tell the player they got got
+would be funny to have another check for number 69 and say "nice"
+
+if number is incorrect, ask whether or not to guess higher or lower than the previous guess
+
+if guess is lower, set max range to be lower than and NOT equal to the previous guess
+
+if guess is higher set the lowest possible range to be higher than and NOT equal to the previous guess */
+
+async function game() {
+  let number = await ask('Choose a number between 1 and 100 (inclusive): ')
+
+  console.log('Did you choose')
+  let answer = await ask
+}
+
+//write a loop that utilizes this function to continue running until the user says that you guessed their number
